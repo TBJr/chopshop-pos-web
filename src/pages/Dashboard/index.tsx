6a0513b0feae9abca097717c37c33fa720066005
TBJ <44341859+TBJr@users.noreply.github.com>
2025-06-09 12:52:06 -0400

    feat(auth): implement Supabase email/password login page and routing

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,19 +1,11 @@
 // src/pages/Dashboard/index.tsx
 import React from "react";
-import BreadCrumb from "../../Components/Common/BreadCrumb";
-import {Container} from "reactstrap";
 
 const Dashboard: React.FC = () => {
-    document.title = "Dashboard | Evolvtech - React Admin & Dashboard Template";
-
     return (
-        <React.Fragment>
-            <div className="page-content">
-                <Container fluid>
-                    <BreadCrumb title="Ecommerce" pageTitle="Dashboard" />
-                </Container>
-            </div>
-        </React.Fragment>
+        <div>
+            {/* …your dashboard JSX… */}
+        </div>
     );
 };
 
